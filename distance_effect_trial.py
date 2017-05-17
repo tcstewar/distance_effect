64549d91fdb8a63c73356a6197d1e46e9fa568f2
Terry Stewart <tcstewar@uwaterloo.ca>
2017-05-17 15:39:39 -0400

    Added rep_mode parameter

--- distance_effect_trial.py
+++ distance_effect_trial.py
@@ -9,7 +9,8 @@ class DistanceEffectTrial(pytry.NengoTrial):
         self.param('number of neurons', n_neurons=100)
         self.param('inter-stimulus interval', isi=0.5)
         self.param('probe synapse', probe_synapse=0.1)
-        
+        self.param('representation mode (concat|convolve)', rep_mode='concat')
+
     def model(self, p):
 
         digits = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE']
@@ -18,43 +19,56 @@ class DistanceEffectTrial(pytry.NengoTrial):
 
         model = nengo.Network()
         with model:
-            
-            
-            ens = nengo.Ensemble(n_neurons=p.n_neurons, dimensions=p.D*2)
-            
-            answer = nengo.Ensemble(n_neurons=100, dimensions=1)
-            
-            
+
+
+            if p.rep_mode == 'concat':
+                dims = p.D * 2
+            elif p.rep_mode == 'convolve':
+                dims = p.D
+            else:
+                raise ValueError('rep_mode must be "concat" or "convolve"')
+
+            ens = nengo.Ensemble(n_neurons=p.n_neurons, dimensions=dims)
+
+            answer = nengo.Ensemble(n_neurons=100, dimensions=1,
+                                    neuron_type=nengo.Direct())
+
+
             inputs = []
             outputs = []
             distance = []
-            self.inputs = inputs 
+            self.inputs = inputs
             self.outputs = outputs
             self.distance = distance
-            
+
             for i in range(len(digits)):
                 for j in range(len(digits)):
                     if i != j:
-                        n1 = vocab.parse(digits[i]).v
-                        n2 = vocab.parse(digits[j]).v
-                        v = np.hstack([n1, n2])
+
+                        if p.rep_mode == 'concat':
+                            n1 = vocab.parse(digits[i]).v
+                            n2 = vocab.parse(digits[j]).v
+                            v = np.hstack([n1, n2])
+                        elif p.rep_mode == 'convolve':
+                            txt = 'OBJ1*%s + OBJ2*%s' % (digits[i], digits[j])
+                            v = vocab.parse(txt).v
                         inputs.append(v)
                         if i < j:
                             outputs.append([-1])
                         else:
                             outputs.append([1])
                         distance.append(abs(i-j))
-                        
+
             nengo.Connection(ens, answer, function=outputs, eval_points=inputs)
-            
+
             def stimulus(t):
                 index = int(t / p.isi) % len(inputs)
                 return self.inputs[index]
             stim = nengo.Node(stimulus)
             nengo.Connection(stim, ens)
-            
+
             self.p_answer = nengo.Probe(answer, synapse=p.probe_synapse)
-            
+
         return model
 
     def evaluate(self, p, sim, plt):
@@ -63,14 +77,14 @@ class DistanceEffectTrial(pytry.NengoTrial):
         self.inputs = np.array(self.inputs)[order]
         self.outputs = np.array(self.outputs)[order]
         self.distance = np.array(self.distance)[order]
-    
+
         sim.run(p.isi * len(self.inputs))
-        
+
         data = sim.data[self.p_answer]
-        
+
         steps = int(p.isi / p.dt)
         final_data = data[steps-1::steps]
-        
+
         error = {
             1: [],
             2: [],
@@ -81,19 +95,19 @@ class DistanceEffectTrial(pytry.NengoTrial):
             7: [],
             8: [],
             }
-            
+
         for i in range(len(final_data)):
             answer = final_data[i]
             correct = self.outputs[i]
             distance = self.distance[i]
-            error[distance].append(abs(float(answer-correct)))    
+            error[distance].append(abs(float(answer-correct)))
 
-        average = {k: np.mean(v) for k,v in error.items()}            
-            
+        average = {k: np.mean(v) for k,v in error.items()}
+
         if plt:
             plt.plot(sim.trange(), data)
-    
+
         return dict(error=error,
                     average=average)
-    
-            
+
+
